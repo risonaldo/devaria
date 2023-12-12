de831adf25de4567b2ef8d2599c5e1cd2303271e
Risonaldo <risonaldomaciel@hotmail.com>
2023-12-12 10:13:31 -0400

    integração jwt ok

--- devagra-backend/pages/api/login.ts
+++ devagra-backend/pages/api/login.ts
@@ -1,13 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { conectarMongoDB } from "../../middleware/conectarMongoDB";
 import type { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
+import type { LoginResposta } from "../../types/LoginResposta";
 import md5 from "md5";
 import { UsuarioModel } from "../../models/UsuarioModel";
+import jwt from "jsonwebtoken";
 
 const endPointLogin = async (
   req: NextApiRequest,
-  res: NextApiResponse<RespostaPadraoMsg>
+  res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
+  const { MINHA_CHAVE_JWT } = process.env;
+  if (!MINHA_CHAVE_JWT) {
+    res.status(500).json({ erro: "ENV Jwt não informado" });
+  }
+
   if (req.method === "POST") {
     const { login, senha } = req.body;
     const usuarioEncontrados = await UsuarioModel.find({
@@ -16,7 +23,13 @@ const endPointLogin = async (
     });
     if (usuarioEncontrados && usuarioEncontrados.length > 0) {
       const usuarioEncontrado = usuarioEncontrados[0];
-      return res.status(200).json({ msg: `Usuario ${usuarioEncontrado.nome} autenticado com sucesso` });
+
+      const token = jwt.sign({ _id: usuarioEncontrado._id }, MINHA_CHAVE_JWT!);
+
+
+      return res.status(200).json({
+        nome: usuarioEncontrado.nome, email: usuarioEncontrado.email, token
+      });
     }
     return res.status(405).json({ erro: "Usuario ou senha Não informado" });
   }
